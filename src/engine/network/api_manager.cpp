08059c4ba235bccc23b789f78b1d80d683870a9d
Keightiie <35301742+Keightiie@users.noreply.github.com>
2025-11-28 02:01:51 +1000

    Restore missing functions preventing compilation

--- src/engine/network/api_manager.cpp
+++ src/engine/network/api_manager.cpp
@@ -50,3 +50,31 @@ QString ApiManager::authorizationKey()
 {
   return QString::fromStdString(config::ConfigUserSettings::stringValue("workshop_key", "PUT_KEY_HERE"));
 }
+
+bool ApiManager::appendFile(QHttpMultiPart *multipart, const QString &fieldName, const QString &filePath)
+{
+  QFile* file = new QFile(filePath);
+  if (!file->open(QIODevice::ReadOnly))
+    return false;
+
+  QHttpPart filePart;
+  filePart.setHeader(QNetworkRequest::ContentDispositionHeader,
+                     QVariant("form-data; name=\"" + fieldName +
+                              "\"; filename=\"" + QFileInfo(*file).fileName() + "\""));
+  filePart.setHeader(QNetworkRequest::ContentTypeHeader, QVariant("application/zip"));
+  filePart.setBodyDevice(file);
+
+  file->setParent(multipart);
+  multipart->append(filePart);
+  return true;
+}
+
+void ApiManager::appendField(QHttpMultiPart *multipart, const QString &name, const QString &value)
+{
+  if(value == "<No Change>" || value.trimmed().isEmpty()) return;
+  QHttpPart part;
+  part.setHeader(QNetworkRequest::ContentDispositionHeader,
+                 QVariant("form-data; name=\"" + name + "\""));
+  part.setBody(value.toUtf8());
+  multipart->append(part);
+}
