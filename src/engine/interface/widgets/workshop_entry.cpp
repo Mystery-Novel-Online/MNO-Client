e9786464089b701149c399da214cebaf8fcb19a2
Keightiie <35301742+Keightiie@users.noreply.github.com>
2026-01-16 11:49:42 +1000

    Tidy up WorkshopEntry class

--- src/engine/interface/widgets/workshop_entry.cpp
+++ src/engine/interface/widgets/workshop_entry.cpp
@@ -8,62 +8,25 @@
 #include <engine/network/api_manager.h>
 #include "engine/fs/fs_reading.h"
 
-WorkshopEntry::WorkshopEntry(int id, const QString &iconPath, const QString &title,
-                             const QString &subtitle, const QString &genderSymbol,
-                             QWidget *parent)
-    : QWidget(parent), m_id(id), m_title(title)
+constexpr int ICON_CONTAINER_SIZE = 50;
+constexpr int ICON_RENDER_SIZE    = 48;
+constexpr int CHILD_INDENT        = 35;
+
+WorkshopEntry::WorkshopEntry(int id, const QString&, const QString &title, const QString &subtitle, const QString&, QWidget *parent) : QWidget(parent), m_id(id), m_title(title)
 {
   setCursor(Qt::PointingHandCursor);
   setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
 
-  m_rootLayout = new QVBoxLayout(this);
-  m_rootLayout->setContentsMargins(0, 0, 0, 0);
-  m_rootLayout->setSpacing(0);
-
-  QWidget *headerWidget = new QWidget(this);
-  mainLayout = new QHBoxLayout(headerWidget);
-  mainLayout->setContentsMargins(1, 1, 1, 1);
-
-  m_IconLabel = new QLabel(headerWidget);
-  m_IconLabel->setFixedSize(50, 50);
-  mainLayout->addWidget(m_IconLabel);
-
-  m_IconUrl = ApiManager::baseUri() + "api/workshop/" + QString::number(id) + "/icon";
-
-  connect(&WorkshopEntry::iconCache(), &WorkshopCache::fileCached, this, &WorkshopEntry::fileDownloaded);
-
-  WorkshopEntry::iconCache().downloadFile(QUrl(m_IconUrl));
-
-  QVBoxLayout *textLayout = new QVBoxLayout();
-  QLabel *titleLabel = new QLabel(title, headerWidget);
-  set_stylesheet(titleLabel, "[WORKSHOP NAME]", COURTROOM_STYLESHEETS_CSS,
-                 AOApplication::getInstance());
-
-  QLabel *subtitleLabel = new QLabel(subtitle, headerWidget);
-  set_stylesheet(subtitleLabel, "[WORKSHOP SUBMITTER]", COURTROOM_STYLESHEETS_CSS,
-                 AOApplication::getInstance());
-
-  textLayout->addWidget(titleLabel);
-  textLayout->addWidget(subtitleLabel);
-  mainLayout->addLayout(textLayout);
-
-  mainLayout->addStretch();
-
-  m_rootLayout->addWidget(headerWidget);
-
-  m_childrenLayout = new QVBoxLayout();
-  m_childrenLayout->setContentsMargins(35, 0, 0, 0);
-  m_rootLayout->addLayout(m_childrenLayout);
-
-  setLayout(m_rootLayout);
+  setupUi(title, subtitle);
+  setupIconDownload();
 }
 
 WorkshopEntry* WorkshopEntry::createChild(int id, const QString &iconPath, const QString &title, const QString &subtitle, const QString &genderSymbol, QWidget *parent)
 {
-  auto* childEntry = new WorkshopEntry(id, iconPath, title, subtitle, genderSymbol, parent);
-  m_childrenLayout->addWidget(childEntry);
-  childEntry->hide();
-  return childEntry;
+  auto* child = new WorkshopEntry(id, iconPath, title, subtitle, genderSymbol, parent);
+  m_childrenLayout->addWidget(child);
+  child->hide();
+  return child;
 }
 
 void WorkshopEntry::fileDownloaded(const QString &filePath, const QString &hash)
@@ -71,57 +34,125 @@ void WorkshopEntry::fileDownloaded(const QString &filePath, const QString &hash)
   if (filePath.isEmpty())
     return;
 
-  QString expectedHash = WorkshopEntry::iconCache().getHashForUrl(m_IconUrl);
-  if (hash == expectedHash)
-  {
-    QPixmap pix;
-    if (pix.load(filePath))
-    {
-      const QString alphaMaskPath = AOApplication::getInstance()->find_theme_asset_path("workshop_alpha.png");
-      if (FS::Checks::FileExists(alphaMaskPath))
-      {
-        QImage base = pix.toImage().convertToFormat(QImage::Format_ARGB32_Premultiplied);
-        QImage mask(alphaMaskPath);
+  if (hash != WorkshopEntry::iconCache().getHashForUrl(m_IconUrl))
+    return;
 
-        if (!mask.isNull())
-        {
-          mask = mask.scaled(base.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation).convertToFormat(QImage::Format_ARGB32_Premultiplied);
+  QPixmap pixmap;
+  if (!pixmap.load(filePath))
+    return;
 
-          QPainter p(&base);
-          p.setCompositionMode(QPainter::CompositionMode_DestinationIn);
-          p.drawImage(0, 0, mask);
-          p.end();
+  applyAlphaMask(pixmap);
 
-          pix = QPixmap::fromImage(base);
-        }
-      }
-
-      m_IconLabel->setPixmap(pix.scaled(48, 48, Qt::KeepAspectRatio, Qt::SmoothTransformation));
-    }
-
-  }
+  m_iconLabel->setPixmap(
+      pixmap.scaled(
+          ICON_RENDER_SIZE,
+          ICON_RENDER_SIZE,
+          Qt::KeepAspectRatio,
+          Qt::SmoothTransformation
+          )
+      );
 }
 
 void WorkshopEntry::mousePressEvent(QMouseEvent *event)
 {
-  if (event->button() == Qt::RightButton) {
+  if (event->button() == Qt::RightButton)
+  {
     emit rightClicked(m_id);
+    return;
   }
-  if (event->button() == Qt::LeftButton) {
+
+  if (event->button() == Qt::LeftButton)
+  {
     emit clicked(m_id);
+    toggleChildrenVisibility(!anyChildrenVisible());
+  }
+}
 
-    bool anyVisible = false;
-    for (int i = 0; i < m_childrenLayout->count(); ++i) {
-      QWidget *w = m_childrenLayout->itemAt(i)->widget();
-      if (w && w->isVisible()) {
-        anyVisible = true;
-        break;
-      }
-    }
+void WorkshopEntry::setupUi(const QString &title, const QString &subtitle)
+{
+  m_rootLayout = new QVBoxLayout(this);
+  m_rootLayout->setContentsMargins(0, 0, 0, 0);
+  m_rootLayout->setSpacing(0);
 
-    for (int i = 0; i < m_childrenLayout->count(); ++i) {
-      QWidget *w = m_childrenLayout->itemAt(i)->widget();
-      if (w) w->setVisible(!anyVisible);
+  auto* headerWidget = new QWidget(this);
+  auto* headerLayout = new QHBoxLayout(headerWidget);
+  headerLayout->setContentsMargins(1, 1, 1, 1);
+
+  m_iconLabel = new QLabel(headerWidget);
+  m_iconLabel->setFixedSize(ICON_CONTAINER_SIZE, ICON_CONTAINER_SIZE);
+  headerLayout->addWidget(m_iconLabel);
+
+  auto* textLayout = new QVBoxLayout();
+
+  auto* titleLabel = new QLabel(title, headerWidget);
+  set_stylesheet(titleLabel, "[WORKSHOP NAME]", COURTROOM_STYLESHEETS_CSS, AOApplication::getInstance());
+
+  auto* subtitleLabel = new QLabel(subtitle, headerWidget);
+  set_stylesheet(subtitleLabel, "[WORKSHOP SUBMITTER]", COURTROOM_STYLESHEETS_CSS, AOApplication::getInstance());
+
+  textLayout->addWidget(titleLabel);
+  textLayout->addWidget(subtitleLabel);
+
+  headerLayout->addLayout(textLayout);
+  headerLayout->addStretch();
+
+  m_rootLayout->addWidget(headerWidget);
+
+  m_childrenLayout = new QVBoxLayout();
+  m_childrenLayout->setContentsMargins(CHILD_INDENT, 0, 0, 0);
+  m_rootLayout->addLayout(m_childrenLayout);
+}
+
+void WorkshopEntry::setupIconDownload()
+{
+  m_IconUrl = ApiManager::baseUri() + "api/workshop/" + QString::number(m_id) + "/icon";
+  connect(&WorkshopEntry::iconCache(), &WorkshopCache::fileCached, this, &WorkshopEntry::fileDownloaded);
+  WorkshopEntry::iconCache().downloadFile(QUrl(m_IconUrl));
+}
+
+void WorkshopEntry::applyAlphaMask(QPixmap &pixmap) const
+{
+  const QString maskPath = AOApplication::getInstance()->find_theme_asset_path("workshop_alpha.png");
+
+  if (!FS::Checks::FileExists(maskPath))
+    return;
+
+  QImage base = pixmap.toImage().convertToFormat(QImage::Format_ARGB32_Premultiplied);
+
+  QImage mask(maskPath);
+
+  if (mask.isNull())
+    return;
+
+  mask = mask
+             .scaled(base.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation)
+             .convertToFormat(QImage::Format_ARGB32_Premultiplied);
+
+  QPainter painter(&base);
+  painter.setCompositionMode(QPainter::CompositionMode_DestinationIn);
+  painter.drawImage(0, 0, mask);
+
+  pixmap = QPixmap::fromImage(base);
+}
+
+bool WorkshopEntry::anyChildrenVisible() const
+{
+  for (int i = 0; i < m_childrenLayout->count(); ++i)
+  {
+    if (auto* w = m_childrenLayout->itemAt(i)->widget();
+        w && w->isVisible())
+    {
+      return true;
     }
+  }
+  return false;
+}
+
+void WorkshopEntry::toggleChildrenVisibility(bool visible)
+{
+  for (int i = 0; i < m_childrenLayout->count(); ++i)
+  {
+    if (auto* w = m_childrenLayout->itemAt(i)->widget())
+      w->setVisible(visible);
   }
 }
--- src/engine/interface/widgets/workshop_entry.h
+++ src/engine/interface/widgets/workshop_entry.h
@@ -8,17 +8,29 @@
 class WorkshopEntry : public QWidget
 {
   Q_OBJECT
+
 public:
-  explicit WorkshopEntry(int id, const QString &iconPath, const QString &title, const QString &subtitle, const QString &genderSymbol, QWidget *parent = nullptr);
-  WorkshopEntry* createChild(int id, const QString &iconPath, const QString &title, const QString &subtitle, const QString &genderSymbol, QWidget *parent = nullptr);
+  explicit WorkshopEntry(
+      int id,
+      const QString &iconPath,
+      const QString &title,
+      const QString &subtitle,
+      const QString &genderSymbol,
+      QWidget *parent = nullptr
+  );
+
+  WorkshopEntry* createChild(
+      int id,
+      const QString &iconPath,
+      const QString &title,
+      const QString &subtitle,
+      const QString &genderSymbol,
+      QWidget *parent = nullptr
+  );
+
   int id() const { return m_id; }
 
-  static WorkshopCache& iconCache() {
-    static WorkshopCache instance("cache_data", "cache_data/icon_cache_db.json");
-    return instance;
-  }
-
-public slots:
+private slots:
   void fileDownloaded(const QString &filePath, const QString &hash);
 
 signals:
@@ -29,16 +41,25 @@ protected:
   void mousePressEvent(QMouseEvent *event) override;
 
 private:
+  static WorkshopCache& iconCache() { static WorkshopCache instance("cache_data", "cache_data/icon_cache_db.json"); return instance; }
+  void setupUi(const QString& title, const QString& subtitle);
+
+  void setupIconDownload();
+  void applyAlphaMask(QPixmap& pixmap) const;
+
+  bool anyChildrenVisible() const;
+  void toggleChildrenVisibility(bool visible);
+
+private:
   int m_id;
   QString m_title;
-
-  QLabel *m_IconLabel = nullptr;
   QString m_IconUrl = "";
 
+  QLabel *m_iconLabel = nullptr;
 
-  QVBoxLayout *m_rootLayout;
-  QHBoxLayout *mainLayout;
-  QVBoxLayout *m_childrenLayout;
+  QVBoxLayout *m_rootLayout = nullptr;
+  QHBoxLayout *mainLayout = nullptr;
+  QVBoxLayout *m_childrenLayout = nullptr;
 };
 
 #endif // WORKSHOP_ENTRY_H
