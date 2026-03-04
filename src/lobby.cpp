74f2d2d5ff238e762de58cf0a005314dc279ca4a
Keightiie <35301742+Keightiie@users.noreply.github.com>
2026-03-04 15:08:53 +1000

    Mod action for workshop deletion

--- src/lobby.cpp
+++ src/lobby.cpp
@@ -215,6 +215,16 @@ Lobby::Lobby(AOApplication *p_ao_app) : SceneWidget(ThemeSceneType::SceneType_Se
 
         ApiManager::instance().post("api/workshop/approve/" + QString::number(id), doc.toJson());
       });
+
+
+      QAction *deleteAction= menu.addAction("Delete");
+      connect(deleteAction, &QAction::triggered, this, [this, id]()
+              {
+                QJsonObject json{{"key", ApiManager::authorizationKey()}};
+                QJsonDocument doc(json);
+
+                ApiManager::instance().post("api/workshop/delete/" + QString::number(id), doc.toJson());
+              });
     }
 
     connect(editAction, &QAction::triggered, this, [this, id]() { WorkshopUploader::StartEdit(id); });
