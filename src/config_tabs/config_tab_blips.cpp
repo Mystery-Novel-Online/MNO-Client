f44c58ad7c29a33ddc20f91ed5ec93f3648d7e9d
Keightiie <35301742+Keightiie@users.noreply.github.com>
2026-05-17 00:03:46 +1000

    feat(blips): populate "blip set" list

--- src/config_tabs/config_tab_blips.cpp
+++ src/config_tabs/config_tab_blips.cpp
@@ -1,10 +1,20 @@
 #include "config_tab_blips.h"
 #include "ui_config_tab_blips.h"
 
+#include <iostream>
+#include <rolechat-lib/src/rolechat/audio/blip_config.h>
+
+#include <rolechat/filesystem/RCDir.h>
+
 config_tab_blips::config_tab_blips(QWidget *parent) : QWidget(parent), ui(new Ui::ConfigTabBlips)
 {
   ui->setupUi(this);
-  ui->blipSet->addItem("<Legacy Blips>");
+  rolechat::fs::RCDir blipDirectory("sounds/blips/");
+  for(const auto& subDir : blipDirectory.subDirectories())
+  {
+    ui->blipSet->addItem(QString::fromStdString(subDir));
+  }
+  BlipConfig blip("danganronpa");
 }
 
 config_tab_blips::~config_tab_blips()
